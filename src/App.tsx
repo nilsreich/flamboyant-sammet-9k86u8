b8ff3b99cecb4973c196f9448b8f475c30b2185c
nilsreich <nilsreich@gmail.com>
2023-04-12 23:10:45 +0000

    Update 5 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,87 @@
-import { useState, useEffect, SyntheticEvent } from "react";
-import { useLocalStorage, useDarkMode } from "usehooks-ts";
-import {
-  Cross2Icon,
-  MoonIcon,
-  SunIcon,
-  PaperPlaneIcon,
-} from "@radix-ui/react-icons";
-
-type TodoType = {
-  id: string;
-  task: string;
-  done: boolean;
-};
+import { useTimer } from "react-timer-hook";
+import { PlayIcon, PauseIcon, ResetIcon } from "@radix-ui/react-icons";
+import { useRef } from "react";
+import * as Slider from "@radix-ui/react-slider";
 
 export default function App() {
-  const [value, setValue] = useState("");
-  const { isDarkMode, toggle, enable, disable } = useDarkMode();
+  const audio = useRef(null);
+  let time = new Date();
+  time.setSeconds(time.getSeconds() + 300);
 
-  const [todo, setTodo] = useLocalStorage<TodoType[]>("todos", []);
+  const { seconds, isRunning, minutes, pause, resume, restart } = useTimer({
+    expiryTimestamp: time,
+    autoStart: false,
+    onExpire: () => audio.current!.play(),
+  });
 
-  useEffect(() => {
-    if (localStorage.getItem("todos") != null) {
-      setTodo(JSON.parse(localStorage.getItem("todos") || "{}"));
-    }
-  }, []);
-
-  const toggleTodo = (id: string) => {
-    let copy = [...todo];
-
-    copy.map((item) => {
-      item.id === id ? (item.done = !item.done) : null;
-    });
-    setTodo([...copy]);
+  const changeTime = (newTime: number) => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + newTime);
+    restart(time);
+    pause();
   };
 
-  const deleteTodo = (id: string) => {
-    let copy = [...todo];
-
-    const index = copy.findIndex((item) => item.id === id);
-
-    copy.splice(index, 1);
-
-    setTodo([...copy]);
-  };
-
-  const addTodo = (e: SyntheticEvent) => {
-    e.preventDefault();
-    if (value.trim() != "") {
-      setTodo([
-        ...todo,
-        {
-          id: crypto.randomUUID(),
-          task: value,
-          done: false,
-        },
-      ]);
-    }
-    setValue(() => "");
-  };
   return (
-    <div className={`${isDarkMode ? "dark" : "light"}`}>
-      <div className="h-screen flex flex-col dark:bg-black dark:text-neutral-500 text-lg">
-        <div className="border-b dark:border-neutral-800 flex justify-end p-1">
-          <button onClick={toggle} className="px-6 py-4">
-            {isDarkMode ? <SunIcon /> : <MoonIcon />}
-          </button>
+    <div className="h-screen flex bg-neutral-950 text-slate-300">
+      <audio src="./water.mp3" ref={audio} />
+      <div className="m-auto">
+        <div className="flex gap-6 items-center">
+          <div className="w-68 text-right py-12 mx-auto text-9xl">
+            {minutes < 10 ? "0" + minutes.toString() : minutes}:
+            {seconds < 10 ? "0" + seconds.toString() : seconds}
+          </div>
+          <div>
+            <form>
+              <Slider.Root
+                className="relative flex flex-col items-center select-none touch-none w-0.5 h-[350px] mt-10"
+                defaultValue={[300]}
+                onValueChange={(e) => changeTime(e[0])}
+                orientation="vertical"
+                max={3599}
+                disabled={isRunning ? true : false}
+                step={60}
+                aria-label="Volume"
+              >
+                <Slider.Track className="bg-neutral-800 w-0.5 grow rounded-lg">
+                  <Slider.Range className="bg-neutral-800 absolute w-full rounded-lg" />
+                </Slider.Track>
+                <Slider.Thumb className="-mt-2 bg-neutral-400 focus:outline-none -ml-2 w-4 rounded-full h-4 absolute" />
+              </Slider.Root>
+            </form>
+          </div>
         </div>
+        <div className="w-min flex mx-auto">
+          {isRunning ? (
+            <button
+              className="bg-black px-4  py-2"
+              onClick={() => {
+                pause();
+              }}
+            >
+              <PauseIcon />
+            </button>
+          ) : (
+            <button
+              className="bg-black px-4 py-2"
+              onClick={() => {
+                resume();
+              }}
+            >
+              <PlayIcon />
+            </button>
+          )}
 
-        <div className="p-4 grow ">
-          {todo.map((item) => {
-            return (
-              <div key={item.id} className="flex mb-4 dark:text-neutral-300">
-                <div
-                  className={`${
-                    item.done
-                      ? "line-through bg-blue-50 dark:bg-slate-950 text-slate-400 dark:text-slate-700"
-                      : "hover:bg-blue-200  bg-blue-100 dark:bg-slate-900"
-                  } hover:dark:bg-slate-800 grow rounded-l px-4 py-2 cursor-pointer	`}
-                  onClick={() => toggleTodo(item.id)}
-                >
-                  {item.task}
-                </div>
-                <button
-                  onClick={() => deleteTodo(item.id)}
-                  className="hover:bg-blue-200 bg-blue-100 dark:bg-slate-900 rounded-r py-1 px-4"
-                >
-                  <Cross2Icon />
-                </button>
-              </div>
-            );
-          })}
-        </div>
-        <form onSubmit={(e) => addTodo(e)} className="flex">
-          <input
-            className="w-full focus:outline-none border-t dark:border-neutral-800 bg-transparent px-4 "
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            type="text"
-          />
-          <button type="submit" className="bg-blue-600 text-white px-6 py-4">
-            <PaperPlaneIcon />
+          <button
+            className="bg-black px-4 py-2 border-l  border-neutral-800"
+            onClick={() => {
+              const time = new Date();
+              time.setSeconds(time.getSeconds() + value);
+              restart(time);
+            }}
+          >
+            <ResetIcon />
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
